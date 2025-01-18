37a2c5cafd8fc6baef0aa0b1b2899b6275f29b03
abdullah <abdullah.softwaredev@gmail.com>
2025-01-18 17:34:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import  {Layout}  from './components/Layout'
+
+function App() {
+ const isAuthenticated = false;
+  return (
+    <Layout>
+
+    </Layout>
+       
+    
+  )
+}
+
+export default App
