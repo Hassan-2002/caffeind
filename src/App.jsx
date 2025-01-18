82e939dbebbefcc51a7a08002f801283aad35f27
abdullah <abdullah.softwaredev@gmail.com>
2025-01-18 18:16:11 +0530

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import  {Layout}  from './components/Layout'
+import { CoffeeForm, Hero, History, Layout, Stats } from "./components";
 
 function App() {
  const isAuthenticated = false;
+ const authenticatedContent = (
+   <>
+   <Stats/>
+   <History/>
+   </>
+ )
   return (
     <Layout>
-
+      <Hero/>
+      <CoffeeForm/>
+      {isAuthenticated && (authenticatedContent)}
     </Layout>
        
     
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,16 @@
 export default function Hero() {
     return (
-        <div>
-            
-        </div>
-    )
+        <>
+          <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or a devotee">Fiends</abbr></h1>  
+          <div className="benefits-list">
+            <h3 className="font-bolder">Try <span className="text-gradient text-center">Caffeind</span> and start...</h3>
+            <p>&#9989; Tracking your coffee</p>
+            <p>&#9989; Measuring your blood caffeine levels</p>
+            <p>&#9989; Costing and quantifying your addiction</p>
+            </div> 
+            <div className="card card-info">
+
+            </div>
+        </>
+    );
 }
